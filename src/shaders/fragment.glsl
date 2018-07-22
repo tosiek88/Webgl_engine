da0381e8b2a58ddc58cf2659a6f99ff0f9a2b528
Mateusz Tocha <MATI50013@wp.pl>
2018-07-22 16:44:29 +0200

    Mistake
    Correct fragment shader has been written

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,10 +1,9 @@
-// an attribute will receive data from a buffer
-attribute vec4 a_position;
+ // fragment shaders don't have a default precision so we need
+  // to pick one. mediump is a good default
+  precision mediump float;
  
-// all shaders have a main function
-void main() {
- 
-  // gl_Position is a special variable a vertex shader
-  // is responsible for setting
-  gl_Position = a_position;
-}
+  void main() {
+    // gl_FragColor is a special variable a fragment shader
+    // is responsible for setting
+    gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+  }
